5274c0c725976d6dd6ce0451d3477d5cc93a6a02
IamPersistent <develop@zestic.com>
2012-02-28 02:41:33 -0800

    initial commit

---
+++ VespolinaPaymentStripeBundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Vespolina\Payment\StripeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class VespolinaPaymentStripeBundle extends Bundle
+{
+
+}
