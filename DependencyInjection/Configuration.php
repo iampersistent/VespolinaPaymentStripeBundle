50d482f60ed7416414854f839d032aee708e3044
IamPersistent <develop@zestic.com>
2012-03-11 17:52:16 -0700

    initial configuration for bundle

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\Payment\StripeBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Richard Shank <develop@zestic.com>
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * Generates the configuration tree.
+     *
+     * @return TreeBuilder
+     */
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('vespolina_payment_stripe');
+        $rootNode
+            ->children()
+                ->scalarNode('api_key')->cannotBeOverwritten()->isRequired()->cannotBeEmpty()->end()
+            ->end()
+        ->end();
+
+        return $treeBuilder;
+    }
+}
---
+++ DependencyInjection/VespolinaPaymentStripeExtension.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * (c) 2012 Vespolina Project http://www.vespolina-project.org
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace Vespolina\Payment\StripeBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+
+use Vespolina\Payment\StripeBundle\DependencyInjection\Configuration;
+
+/**
+ * @author Richard D Shank <develop@zestic.com>
+ */
+class VespolinaPaymentStripeExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $processor = new Processor();
+        $configuration = new Configuration();
+
+        $config = $processor->processConfiguration($configuration, $configs);
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        $loader->load('services.xml');
+
+        $container->setParameter('vespolina.payment.stripe.api.key', $config['api_key']);
+    }
+}
